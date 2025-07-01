17479c4dc02201a72645c67aebd505f31a19bbda
freedomer11 <bogdanzozyliya@gmail.com>
2025-07-01 19:26:26 +0300

    Variables and Data Types

---
+++ Variables and Data Types/VariablesAndDataTypes.js
@@ -0,0 +1,11 @@
+let userName = "Bohdan";
+console.log(userName);
+
+let userAge = 26;
+console.log(String(userAge));
+
+let num = 10;
+let numStr = '20';
+let concatenate = num + numStr;
+
+console.log('Значення:',concatenate, ' Тип:',typeof concatenate)
