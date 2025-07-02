f68dd71e10f79a8068e6e586a7ae973d8d727629
freedomer11 <bogdanzozyliya@gmail.com>
2025-07-02 19:54:47 +0300

    Array

---
+++ Arrays/Arrays.js
@@ -0,0 +1,9 @@
+let arr = ['Alex', 'Cristina', 'Tina'];
+arr.push('Jon');
+console.log(arr);
+
+let firstElement = arr.shift();
+console.log(firstElement)
+
+let arrR = ["Mike", "John", "Sara"];
+console.log(arrR.indexOf("John"))
